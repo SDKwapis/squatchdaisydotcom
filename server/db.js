d4e49a90e3987bf25e0aded5ee024011d4b2f1b5
sdkwapis <sdkwapis@gmail.com>
2025-06-07 12:49:36 -0400

    deploying to render, fixing bugs

--- server/db.js
+++ server/db.js
@@ -1,9 +1,17 @@
+require('dotenv').config(); // Add this at the top
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('squatchdaisy_db', 'your_pg_username', 'your_pg_password', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+  }
+);
 
 module.exports = sequelize;
+
 
